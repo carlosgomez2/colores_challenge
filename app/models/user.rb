e4c7ee6b8f77163236f6b8a42e2bf9fe3a16125c
Carlos Gómez <carlosgomez.deb@gmail.com>
2021-04-02 15:07:54 -0600

    :art: - expiry token in 1 day

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   def generate_jwt
     JWT.encode(
-      { id: id, exp: 60.days.from_now.to_i },
+      { id: id, exp: 1.days.from_now.to_i },
       Rails.application.secrets.secret_key_base
     )
   end
